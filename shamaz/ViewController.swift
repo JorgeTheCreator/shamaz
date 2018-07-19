783386520253c8853c7e13ed742ef56197df85e3
JorgeTheCreator <JrgPeguero@yahoo.com>
2018-07-19 18:15:09 -0400

    Initial Commit

---
+++ shamaz/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  shamaz
+//
+//  Created by Jorge Peguero on 7/17/18.
+//  Copyright © 2018 Jorge Peguero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var randomPlayerNumber : Int = 0
+    let myRedColor = UIColor(
+        white: 1.0, alpha: 0.8)
+    let myColor = UIColor(red:0.43, green:0.56, blue:0.75, alpha:1.0)
+    let futureArray = ["What would you like to be doing a month from now?","What would you like to be doing 2 weeks from now?","What would you like to be doing a year from now?"]
+
+    @IBOutlet weak var textField: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+     
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func whosNext(_ sender: UIButton) {
+        randomPlayerNumber = Int(arc4random_uniform(10)+1)
+        randomNumberGenerator()
+        sender.shake()
+    }
+    
+    @IBAction func pastExperience(_ sender: UIButton) {
+         sender.shake()
+    }
+    @IBAction func futurePlan(_ sender: UIButton) {
+        textField.text =  futureArray[Int(arc4random_uniform(UInt32(futureArray.count)))]
+        textField.font =  UIFont(name: "HelveticaNeue-UltraLight", size: 40 )
+        textField.textColor = randomColor()
+        textField.backgroundColor = myRedColor
+        view.backgroundColor = myColor
+        sender.shake()
+        
+    }
+    
+    func randomNumberGenerator(){
+        textField.text =  "Player \(randomPlayerNumber)"
+        textField.font =  UIFont(name: "HelveticaNeue-UltraLight", size: 100)
+        textField.textAlignment = .center
+        view.backgroundColor = randomColor()
+        textField.backgroundColor = randomColor()
+     
+    }
+    func randomColor() -> UIColor{
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    
+}
+
