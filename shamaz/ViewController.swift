7125b4484d5a455eec68f05e3cbe89e20a5e40e2
JorgeTheCreator <JrgPeguero@yahoo.com>
2018-07-20 17:11:39 -0400

    add constranst, pastExperience Array

--- shamaz/ViewController.swift
+++ shamaz/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     let myRedColor = UIColor(
         white: 1.0, alpha: 0.8)
     let myColor = UIColor(red:0.43, green:0.56, blue:0.75, alpha:1.0)
-    let futureArray = ["What would you like to be doing a month from now?","What would you like to be doing 2 weeks from now?","What would you like to be doing a year from now?"]
+    let futureArray = ["What would you like to be doing a month from now?","What would you like to be doing 2 weeks from now?","What would you like to be doing 3 days from now?"]
+    let pastExperiences = ["what did you do last week?", "what did you do yesterday", "What did you do last monday","What did you do last weekend"]
 
     @IBOutlet weak var textField: UILabel!
     
@@ -36,21 +37,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func pastExperience(_ sender: UIButton) {
+        textField.text =  pastExperiences[Int(arc4random_uniform(UInt32(pastExperiences.count)))]
+        textField.font =  UIFont(name: "HelveticaNeue-UltraLight", size: 40 )
+        textField.textAlignment = .center
+        textField.textColor = randomColor()
+        textField.backgroundColor = myRedColor
+        view.backgroundColor = myColor
+        
          sender.shake()
     }
     @IBAction func futurePlan(_ sender: UIButton) {
         textField.text =  futureArray[Int(arc4random_uniform(UInt32(futureArray.count)))]
-        textField.font =  UIFont(name: "HelveticaNeue-UltraLight", size: 40 )
+        textField.font =  UIFont(name: "HelveticaNeue-UltraLight", size: 35 )
         textField.textColor = randomColor()
         textField.backgroundColor = myRedColor
         view.backgroundColor = myColor
+        textField.textAlignment = .center
         sender.shake()
         
     }
     
     func randomNumberGenerator(){
         textField.text =  "Player \(randomPlayerNumber)"
-        textField.font =  UIFont(name: "HelveticaNeue-UltraLight", size: 100)
+        textField.font =  UIFont(name: "HelveticaNeue-UltraLight", size: 90)
         textField.textAlignment = .center
         view.backgroundColor = randomColor()
         textField.backgroundColor = randomColor()
